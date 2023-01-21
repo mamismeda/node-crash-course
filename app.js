76f3b8569302867a3f023906ed12e36f76485c75
mamismeda <amamismeda@gmail.com>
2023-01-21 11:46:01 +0400

    added mongoose and mongodb to project

--- app.js
+++ app.js
@@ -1,29 +1,54 @@
 const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
 
 // express app
 
 const app = express();
 
+// connect to mongodb
+const dbURI =
+  "mongodb+srv://netninja:test12345@cluster0.sbyayzw.mongodb.net/node-tuts?retryWrites=true&w=majority";
+
+mongoose.connect(dbURI);
+
+// register view engine
+app.set("view engine", "ejs");
+
 // listen for requests
 
 app.listen(3000);
 
+app.use(morgan("dev"));
+
 app.get("/", (req, res) => {
-  // res.send('<p> home page </p>');
-  res.sendFile("./views/index.html", { root: __dirname });
+  const blogs = [
+    {
+      title: "Yoshi finds eggs",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "Mario finds stars",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "How to defeat bowser",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+  ];
+  res.render("index", { title: "Home", blogs });
 });
 
 app.get("/about", (req, res) => {
   // res.send('<p> about page </p>');
-  res.sendFile("./views/about.html", { root: __dirname });
+  res.render("about", { title: "About" });
 });
 
-// redirects
-app.get("/about-us", (req, res) => {
-  res.redirect("/about");
+app.get("/blogs/create", (req, res) => {
+  res.render("create", { title: "Create a new Blog" });
 });
 
 // 404page
-app.use((req,res) => {
-    res.status(404).sendFile("./views/404.html", { root: __dirname });
-})
+app.use((req, res) => {
+  res.status(404).render("404", { title: "404" });
+});
